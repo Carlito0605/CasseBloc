2c4bb12c3561078abfd64507b0b15cd6aa6d79f4
Charles <charlescretois@gmail.com>
2022-10-14 13:44:13 +0200

    First Commit

---
+++ cmake-build-debug/CMakeFiles/CasseBloc.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CasseBloc.dir/main.c.obj"
+  "CasseBloc.exe"
+  "CasseBloc.exe.manifest"
+  "CasseBloc.lib"
+  "CasseBloc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CasseBloc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
