fc992bd0f0fe0eb4d941f8604e6e2d7e37f89733
Charles <charlescretois@gmail.com>
2022-10-19 16:56:23 +0200

    Explosions en chaînes + meilleur affichage

--- bombs.h
+++ bombs.h
@@ -1,12 +1,28 @@
 #include "main.h"
 
 void displayMap(char**map, int rows, int columns){
-    for(int i = 0; i<rows; i++){
-        for(int y = 0; y<columns; y++){
-            printf("%c ",map[i][y]);
-        }
-        printf("\n");
+    for(int i = -1; i<columns; i++){
+        if(i!=-1) printf("%2d ",i);
+        else printf("   ");
     }
+    printf("\n");
+    for(int i = -1; i<columns; i++){
+        if(i!=-1) printf("__ ");
+        else printf("   ");
+    }
+    printf("\n");
+    for(int i = 0; i<rows; i++){
+        printf("%d | ",i);
+        for(int y = 0; y<columns; y++){
+            printf("%c  ",map[i][y]);
+        }
+        printf("|\n");
+    }
+    for(int i = -1; i<columns; i++){
+        if(i!=-1) printf("__ ");
+        else printf("   ");
+    }
+    printf("\n");
 }
 
 char** bombExplosion(char**map, int rows, int columns, int v_pos, int h_pos, int range){
@@ -14,10 +30,42 @@ char** bombExplosion(char**map, int rows, int columns, int v_pos, int h_pos, int
     map[v_pos][h_pos] = 'X';
 
     for(int i = 1; i<=range; i++){
-        if((v_pos+i) < rows) map[v_pos+i][h_pos] = '*';
-        if((v_pos-i) >= 0) map[v_pos-i][h_pos] = '*';
-        if((h_pos+i) < columns) map[v_pos][h_pos+i] = '*';
-        if((h_pos-i) >= 0) map[v_pos][h_pos-i] = '*';
+        if((v_pos+i) < rows){
+            if(map[v_pos+i][h_pos] != '*' && map[v_pos+i][h_pos] != 'X' && map[v_pos+i][h_pos] != '.'){
+                map = bombExplosion(map,rows,columns,v_pos+i,h_pos,2);
+                map[v_pos+i][h_pos] = 'X';
+            }
+            else if(map[v_pos+i][h_pos] != 'X' && map[v_pos+i][h_pos] != '*'){
+                map[v_pos+i][h_pos] = '*';
+            }
+        }
+        if((v_pos-i) >= 0){
+            if(map[v_pos-i][h_pos] != '*' && map[v_pos-i][h_pos] != 'X' && map[v_pos-i][h_pos] != '.'){
+                map = bombExplosion(map,rows,columns,v_pos-i,h_pos,2);
+                map[v_pos-i][h_pos] = 'X';
+            }
+            else if(map[v_pos-i][h_pos] != 'X' && map[v_pos-i][h_pos] != '*'){
+                map[v_pos-i][h_pos] = '*';
+            }
+        }
+        if((h_pos+i) < columns){
+            if(map[v_pos][h_pos+i] != '*' && map[v_pos][h_pos+i] != 'X' && map[v_pos][h_pos+i] != '.'){
+                map = bombExplosion(map,rows,columns,v_pos,h_pos+i,2);
+                map[v_pos][h_pos+i] = 'X';
+            }
+            else if(map[v_pos][h_pos+i] != 'X' && map[v_pos][h_pos+i] != '*'){
+                map[v_pos][h_pos+i] = '*';
+            }
+        }
+        if((h_pos-i) >= 0){
+            if(map[v_pos][h_pos-i] != '*' && map[v_pos][h_pos-i] != 'X' && map[v_pos][h_pos-i] != '.'){
+                map = bombExplosion(map,rows,columns,v_pos,h_pos-i,2);
+                map[v_pos][h_pos-i] = 'X';
+            }
+            else if(map[v_pos][h_pos-i] != 'X' && map[v_pos][h_pos-i] != '*'){
+                map[v_pos][h_pos-i] = '*';
+            }
+        }
     }
 
     return map;
@@ -65,6 +113,8 @@ void test(int rows, int columns, Player* players){
     map[9][11] = '5';
     map[5][14] = '5';
     map[6][9] = '5';
+    map[2][6] = '5';
+    map[4][2] = '5';
 
     displayMap(map,rows,columns);
 
