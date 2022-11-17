26648bdbb77c53cec63a4aefaf3bd07b19d19a66
Charles <charlescretois@gmail.com>
2022-11-17 23:46:04 +0100

    BombKick activation !!!

--- main.c
+++ main.c
@@ -151,25 +151,13 @@ void game(int player_size){
     for(int i = 0; i<rows; i++){
         for(int y = 0; y<columns; y++){
             map[i][y] = ' ';
-            //if(i == 0 || i == rows-1 || y == 0 || y == columns-1) map[i][y] = 'x';
+            if(i == 0 || i == rows-1 || y == 0 || y == columns-1) map[i][y] = 'x';
             if(i == 9 || i == 10 || y == 9 || y == 10) map[i][y] = 'm';
         }
     }
-    map[0][0] = 'A';
-    map[0][19] = '1';
+    map[3][3] = '1';
     map[17][3] = '2';
     map[2][16] = '3';
-
-    //TEST PLACEMENT DES BOMBES
-    map[3][3] = 'A';
-    map[3][4] = 'A';
-    map[16][2] = 'D';
-    map[15][3] = 'G';
-    map[16][4] = 'J';
-    map[2][15] = 'D';
-    map[1][16] = 'G';
-    map[2][17] = 'J';
-    //END TEST
 
     displayMap(map,rows,columns);
 
@@ -182,7 +170,7 @@ void game(int player_size){
             .range = 2,
             .current_bombs = 2,
             .invincibility = 0, //isn't invincible yet
-            .bomb_special_power_up = 0, //don't have any special power up
+            .bomb_special_power_up = 1, //don't have any special power up
             .heart=0, //didn't get a heart this game
             .bombs_car="ABC",
             .dead = 0,
@@ -239,11 +227,6 @@ void game(int player_size){
 
     for(int i = 0; i<player_size; i++) findPlayerOnTheMap(map, rows, columns, i, players);
 
-    //TEST checkBombKick() en donnant BombKick à tous les joueurs
-    for(int i=0; i<4; i++) {
-        players[i].bomb_special_power_up = 1;
-    }
-
     ///START OF TURNS
 
     int is_playing = 1;
@@ -267,38 +250,30 @@ void game(int player_size){
 
         if(players[who_is_playing-1].dead) who_is_playing++;
 
-        //TEST checkBombKick
-        int checkR = checkBombKick(map, players[who_is_playing-1], 'R');
-        int checkL = checkBombKick(map, players[who_is_playing-1], 'L');
-        int checkT = checkBombKick(map, players[who_is_playing-1], 'T');
-        int checkB = checkBombKick(map, players[who_is_playing-1], 'B');
-        if(checkR) {
-            printf("Peut utiliser Bomb Kick à droite\n");
-        }
-        if(checkL) {
-            printf("Peut utiliser Bomb Kick à gauche\n");
-        }
-        if(checkT) {
-            printf("Peut utiliser Bomb Kick en haut\n");
-        }
-        if(checkB) {
-            printf("Peut utiliser Bomb Kick en bas\n");
-        }
-        if(!(checkR || checkL || checkT || checkB)) {
-            printf("Ne peut pas utiliser Bomb Kick\n");
-        }
-        //END TEST
-
         printf("\nACTIONS : ");
         printf("\nC'est au joueur %d de jouer !\n",who_is_playing);
-        printf("- Flèche de droite si tu veux aller a droite\n");
-        printf("- Flèche de gauche si tu veux aller a gauche\n");
-        printf("- Flèche du bas si tu veux aller bas\n");
-        printf("- Flèche du haut si tu veux aller haut\n");
-        printf("- Appuie sur '1' si tu veux poser une bombe à droite\n");
-        printf("- Appuie sur '2' si tu veux poser une bombe à gauche\n");
+        printf("- Fleche de droite si tu veux aller a droite\n");
+        printf("- Fleche de gauche si tu veux aller a gauche\n");
+        printf("- Fleche du bas si tu veux aller bas\n");
+        printf("- Fleche du haut si tu veux aller haut\n");
+        printf("- Appuie sur '1' si tu veux poser une bombe a droite\n");
+        printf("- Appuie sur '2' si tu veux poser une bombe a gauche\n");
         printf("- Appuie sur '3' si tu veux poser une bombe en haut\n");
         printf("- Appuie sur '4' si tu veux poser une bombe en bas\n");
+
+        if(checkBombKick(map, players[who_is_playing-1], 'R')) {
+            printf("- Appuie sur '5' si tu kick une bombe a droite\n");
+        }
+        if(checkBombKick(map, players[who_is_playing-1], 'L')) {
+            printf("- Appuie sur '6' si tu kick une bombe a gauche\n");
+        }
+        if(checkBombKick(map, players[who_is_playing-1], 'T')) {
+            printf("- Appuie sur '7' si tu kick une bombe en haut\n");
+        }
+        if(checkBombKick(map, players[who_is_playing-1], 'B')) {
+            printf("- Appuie sur '8' si tu kick une bombe en bas\n");
+        }
+
         printf("- Appuie sur '0' si tu veux quitter la partie.\n");
         printf("\nInput : ");
 
@@ -346,6 +321,18 @@ void game(int player_size){
                 break;
             case 52 : // -> 4 -> Drop a bomb under
                 map = dropBomb(map, players[who_is_playing-1].v_pos+1,players[who_is_playing-1].h_pos,&players[who_is_playing-1],players[who_is_playing-1].bombs_car);
+                break;
+            case 53 : // -> 5 -> Kick a bomb to the right
+                if(checkBombKick(map, players[who_is_playing-1], 'R')) map = bombKick(map,rows,columns,'R',players[who_is_playing-1].v_pos,players[who_is_playing-1].h_pos+1);
+                break;
+            case 54 : // -> 6 -> Kick a bomb to the left
+                if(checkBombKick(map, players[who_is_playing-1], 'L')) map = bombKick(map,rows,columns,'L',players[who_is_playing-1].v_pos,players[who_is_playing-1].h_pos-1);
+                break;
+            case 55 : // -> 7 -> Kick a bomb to the top
+                if(checkBombKick(map, players[who_is_playing-1], 'T')) map = bombKick(map,rows,columns,'T',players[who_is_playing-1].v_pos-1,players[who_is_playing-1].h_pos);
+                break;
+            case 56 : // -> 8 -> Kick a bomb to the bottom
+                if(checkBombKick(map, players[who_is_playing-1], 'B')) map = bombKick(map,rows,columns,'B',players[who_is_playing-1].v_pos+1,players[who_is_playing-1].h_pos);
                 break;
             case 32 : // -> space
                 break;
